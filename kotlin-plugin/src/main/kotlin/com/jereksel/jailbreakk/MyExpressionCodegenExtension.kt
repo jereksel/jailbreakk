0f6c7bde3acff7c1cebea564724218b1a6bf066e
Andrzej Ressel <jereksel@gmail.com>
2019-11-27 21:13:27 +0100

    Primitive boolean support

--- kotlin-plugin/src/main/kotlin/com/jereksel/jailbreakk/MyExpressionCodegenExtension.kt
+++ kotlin-plugin/src/main/kotlin/com/jereksel/jailbreakk/MyExpressionCodegenExtension.kt
@@ -1,19 +1,45 @@
 package com.jereksel.jailbreakk
 
-import jdk.internal.org.objectweb.asm.commons.GeneratorAdapter
 import org.jetbrains.kotlin.codegen.OperationStackValue
 import org.jetbrains.kotlin.codegen.StackValue
 import org.jetbrains.kotlin.codegen.asmType
 import org.jetbrains.kotlin.codegen.extensions.ExpressionCodegenExtension
-import org.jetbrains.kotlin.resolve.calls.callUtil.getFirstArgumentExpression
-import org.jetbrains.kotlin.resolve.calls.callUtil.getParameterForArgument
+import org.jetbrains.kotlin.codegen.state.KotlinTypeMapper
 import org.jetbrains.kotlin.resolve.calls.callUtil.getReceiverExpression
 import org.jetbrains.kotlin.resolve.calls.inference.returnTypeOrNothing
 import org.jetbrains.kotlin.resolve.calls.model.ResolvedCall
-import org.jetbrains.kotlin.types.typeUtil.makeNullable
+import org.jetbrains.kotlin.types.KotlinType
 import org.jetbrains.org.objectweb.asm.Type
+import org.jetbrains.org.objectweb.asm.commons.InstructionAdapter
 
 class MyExpressionCodegenExtension : ExpressionCodegenExtension {
+
+    val CLASS_TYPE = Type.getType(Class::class.java)
+
+    companion object {
+        val BOOLEAN_OBJECT_TYPE = Type.getType(java.lang.Boolean::class.java)!!
+        val BYTE_OBJECT_TYPE = Type.getType(java.lang.Byte::class.java)!!
+        val CHAR_OBJECT_TYPE = Type.getType(java.lang.Character::class.java)!!
+        val DOUBLE_OBJECT_TYPE = Type.getType(java.lang.Double::class.java)!!
+        val FLOAT_OBJECT_TYPE = Type.getType(java.lang.Float::class.java)!!
+        val INT_OBJECT_TYPE = Type.getType(java.lang.Integer::class.java)!!
+        val LONG_OBJECT_TYPE = Type.getType(java.lang.Long::class.java)!!
+        val SHORT_OBJECT_TYPE = Type.getType(java.lang.Short::class.java)!!
+
+        val primitiveToWrapper: Map<Type, Type> = mapOf(
+                Type.BOOLEAN_TYPE to BOOLEAN_OBJECT_TYPE,
+                Type.BYTE_TYPE to BYTE_OBJECT_TYPE,
+                Type.CHAR_TYPE to CHAR_OBJECT_TYPE,
+                Type.DOUBLE_TYPE to DOUBLE_OBJECT_TYPE,
+                Type.FLOAT_TYPE to FLOAT_OBJECT_TYPE,
+                Type.INT_TYPE to INT_OBJECT_TYPE,
+                Type.LONG_TYPE to LONG_OBJECT_TYPE,
+                Type.SHORT_TYPE to SHORT_OBJECT_TYPE
+        )
+
+        fun Type.toWrapper(): Type = primitiveToWrapper[this] ?: error("Type $this is not primitive type")
+
+    }
 
     override fun applyFunction(receiver: StackValue, resolvedCall: ResolvedCall<*>, c: ExpressionCodegenExtension.Context): StackValue? {
         println("FUNCTION")
@@ -41,9 +67,8 @@ class MyExpressionCodegenExtension : ExpressionCodegenExtension {
 
             candidateDescriptor.valueParameters.forEachIndexed { i, valueParameterDescriptor ->
                 it.dup()
-                val type = valueParameterDescriptor.type.asmType(c.typeMapper)
                 it.iconst(i)
-                it.aconst(type)
+                it.putType(valueParameterDescriptor.type, c.typeMapper)
                 it.astore(classType)
             }
 
@@ -61,13 +86,60 @@ class MyExpressionCodegenExtension : ExpressionCodegenExtension {
             parameters.forEachIndexed { i, stackValue ->
                 it.dup()
                 it.iconst(i)
-                stackValue.put(it)
-                it.astore(stackValue.type)
+                it.putStackValue(stackValue)
+                it.astore(objectType)
             }
 
             it.invokevirtual("java/lang/reflect/Method", "invoke", "(Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;", false)
-            it.checkcast(candidateDescriptor.returnTypeOrNothing.asmType(c.typeMapper))
+            it.castObject(candidateDescriptor.returnTypeOrNothing, c.typeMapper)
         }
+    }
+
+    private fun InstructionAdapter.putType(kotlinType: KotlinType, typeMapper: KotlinTypeMapper) {
+
+        when(val type = kotlinType.asmType(typeMapper)) {
+            Type.BOOLEAN_TYPE -> putPrimitiveType(Type.BOOLEAN_TYPE)
+            Type.BYTE_TYPE -> putPrimitiveType(Type.BYTE_TYPE)
+            Type.CHAR_TYPE -> putPrimitiveType(Type.CHAR_TYPE)
+            Type.INT_TYPE -> putPrimitiveType(Type.INT_TYPE)
+            Type.FLOAT_TYPE -> putPrimitiveType(Type.FLOAT_TYPE)
+            Type.DOUBLE_TYPE -> putPrimitiveType(Type.DOUBLE_TYPE)
+            Type.LONG_TYPE -> putPrimitiveType(Type.LONG_TYPE)
+            else -> aconst(type)
+        }
+
+    }
+
+    private fun InstructionAdapter.putPrimitiveType(primitiveType: Type) {
+        getstatic(primitiveType.toWrapper().internalName, "TYPE", CLASS_TYPE.descriptor)
+    }
+
+    private fun InstructionAdapter.putStackValue(stackValue: StackValue) {
+
+        stackValue.put(this)
+
+        if (stackValue.type == Type.BOOLEAN_TYPE) {
+            invokestatic("java/lang/Boolean", "valueOf", "(Z)Ljava/lang/Boolean;", false)
+        }
+
+    }
+
+    private fun InstructionAdapter.castObject(destType: KotlinType, typeMapper: KotlinTypeMapper) {
+
+        val type = destType.asmType(typeMapper)
+
+        if (type == Type.BOOLEAN_TYPE) {
+            checkcast(BOOLEAN_OBJECT_TYPE)
+            invokevirtual("java/lang/Boolean", "booleanValue", "()Z", false)
+        } else {
+            checkcast(type)
+        }
+
+    }
+
+    fun a() {
+        val m = Class::class.java.getMethod("a")
+        m.invoke(null, true)
     }
 
 }
--- kotlin-plugin/src/test/kotlin/com/jereksel/jailbreakk/PrimitiveTest.kt
+++ kotlin-plugin/src/test/kotlin/com/jereksel/jailbreakk/PrimitiveTest.kt
@@ -1,23 +1,49 @@
 package com.jereksel.jailbreakk
 
+import com.tschuchort.compiletesting.SourceFile
+
 class PrimitiveTest : AbstractCompilerTest() {
 
     init {
 
         "Primitive types" {
 
-            class SecretClass() {
-                private fun getBoolean(n: Boolean) = n
-                private fun getByte(n: Byte) = n
-                private fun getChar(n: Char) = n
-                private fun getShort(n: Short) = n
-                private fun getInt(n: Int) = n
-                private fun getLong(n: Long) = n
-                private fun getFloat(n: Float) = n
-                private fun getDouble(n: Double) = n
-            }
+            val kotlinSource = SourceFile.kotlin("Main.kt", """
+                import jb.SecretClass.*
 
+                class SecretClass {
+                    private fun getBoolean(n: Boolean) = n
+                    private fun getByte(n: Byte) = n
+                    private fun getChar(n: Char) = n
+                    private fun getShort(n: Short) = n
+                    private fun getInt(n: Int) = n
+                    private fun getLong(n: Long) = n
+                    private fun getFloat(n: Float) = n
+                    private fun getDouble(n: Double) = n
+                }
+                
+                fun test(): String {
+                    val clz = SecretClass()
+                    
+                    if (clz.getBoolean(false)) {
+                        return "FAIL"
+                    }
+                    
+//                    val b = clz.getByte(1)
+//                    val c = clz.getChar(2.toChar())
+//                    val s = clz.getShort(3)
+//                    val i = clz.getInt(4)
+//                    val l = clz.getLong(5L)
+//                    val float = clz.getFloat(6.0F)
+//                    val double = clz.getDouble(7.0)
+                    
+                    return "OK"
+                
+                }
+            
+            """)
 
+            testCompilation(kotlinSource)
 
         }
 
