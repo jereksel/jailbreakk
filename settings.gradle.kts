a091d7fa06604732bc4b169411f581975450eeb3
Andrzej Ressel <jereksel@gmail.com>
2019-12-14 21:54:02 +0100

    Gradle build scans (#8)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,20 @@
+plugins {
+    id("com.gradle.enterprise").version("3.1.1")
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+
+        obfuscation {
+            username { "HIDDEN" }
+            hostname { "HIDDEN" }
+            ipAddresses { emptyList() }
+        }
+    }
+}
+
 include(
     ":gradle-plugin",
     ":ide-plugin",
